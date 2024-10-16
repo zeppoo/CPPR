08882a4f9fc1a1e2281453111da3a99af692b761
Rein Verhaag <reinverhaag@gmail.com>
2024-10-16 22:37:40 +0200

    Rep init

---
+++ CPPR/include/library.h
@@ -0,0 +1,6 @@
+#ifndef CPPR_LIBRARY_H
+#define CPPR_LIBRARY_H
+
+void hello();
+
+#endif //CPPR_LIBRARY_H
---
+++ CPPR/library.cpp
@@ -0,0 +1,8 @@
+#include "include/library.h"
+
+#include <iostream>
+
+void hello()
+{
+  std::cout << "Hello, World!" << std::endl;
+}
